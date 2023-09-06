054019fa81efb5712387f7d2cf7bdc8340e6f741
Trunk <Jesdakorns@gmail.com>
2023-09-06 16:50:10 +0700

    no message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-// import "@fontsource/roboto/300.css";
-// import "@fontsource/roboto/400.css";
-// import "@fontsource/roboto/500.css";
-// import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import ThemeRegistry from "./ThemeRegistry";
 
 export const metadata: Metadata = {
